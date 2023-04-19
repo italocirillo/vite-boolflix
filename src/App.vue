9af4505c09aa737cd6307a744cfde7365675949c
italocirillo <italocirillo@gmail.com>
2023-04-19 21:01:39 +0200

    Collegamento ad axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import axios from "axios";
+import { store } from "./store";
 
 export default {
-  components: { AppHeader, AppMain }
+  components: { AppHeader, AppMain },
+  data() {
+    return {
+      store
+    }
+  },
+  mounted() {
+    this.ottieniFilm();
+  },
+
+  methods: {
+    ottieniFilm() {
+
+      const params = {
+        api_key: "98f46d7d18a354762b374158a8da360e",
+        query: "Vita",
+      };
+
+      axios.get(this.store.apiUrl, {
+        params
+      }).then((resp) => {
+        console.log(resp.data);
+      })
+
+    }
+  }
 }
 
 </script>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    catalogoFilm: [],
+    catalogoSerie: [],
+    apiUrl: "https://api.themoviedb.org/3/search/movie",
+    loading: false,
+});
