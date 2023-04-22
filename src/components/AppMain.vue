b1ceb42cda61ab66cbe16e032abf5d94ecb02c98
italocirillo <italocirillo@gmail.com>
2023-04-22 03:16:32 +0200

    nessun film

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -24,31 +24,36 @@ export default {
 </script>
 
 <template>
-    <!-- LOADING -->
-    <div v-if="this.store.loading" class="container">
-        <p>Loading... <i class="fa-sharp fa-solid fa-spinner"></i></p>
+    <div v-if="catalogoFilm.length === 0 || catalogoSerie === 0" class="nessun-film">
+        <h1>Cerca un film o una serie</h1>
     </div>
+    <div v-else>
+        <!-- LOADING -->
+        <div v-if="this.store.loading" class="container">
+            <p>Loading... <i class="fa-sharp fa-solid fa-spinner"></i></p>
+        </div>
 
-    <div v-else class="container">
-        <!-- FILM -->
-        <div>
-            <h2 v-if="catalogoFilm.length > 0" class="text-start p-3">FILM Trovati: {{
-                catalogoFilm.length
-            }}</h2>
-            <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-1 g-3">
-                <div class="col p-1" v-for="film in catalogoFilm" :key="film.id">
-                    <CardFilm :film="film" />
+        <div v-else class="container">
+            <!-- FILM -->
+            <div>
+                <h2 v-if="catalogoFilm.length > 0" class="text-start p-3">FILM Trovati: {{
+                    catalogoFilm.length
+                }}</h2>
+                <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-1 g-3">
+                    <div class="col p-1" v-for="film in catalogoFilm" :key="film.id">
+                        <CardFilm :film="film" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- SERIE TV  -->
-        <div>
-            <h2 v-if="catalogoSerie.length > 0" class="text-start p-3">SERIE Trovate: {{
-                catalogoSerie.length
-            }}</h2>
-            <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-1 g-3">
-                <div class="col p-1" v-for="film in catalogoSerie" :key="film.id">
-                    <CardFilm :film="film" />
+            <!-- SERIE TV  -->
+            <div>
+                <h2 v-if="catalogoSerie.length > 0" class="text-start p-3">SERIE Trovate: {{
+                    catalogoSerie.length
+                }}</h2>
+                <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-1 g-3">
+                    <div class="col p-1" v-for="film in catalogoSerie" :key="film.id">
+                        <CardFilm :film="film" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -56,6 +61,15 @@ export default {
 </template>
 
 <style lang="scss">
+.nessun-film {
+    padding: 20em 0em;
+    text-align: center;
+
+    h1 {
+        text-transform: uppercase;
+    }
+}
+
 .container {
     padding-top: 8em;
 }
